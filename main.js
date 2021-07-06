787b72c23256ac9d1c8d57e9f185cdd1270789e3
Gurmeet Singh <74553130+gurmeet-S@users.noreply.github.com>
2021-07-06 17:17:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 img = "";
+status = "";
 function preload(){
 img = loadImage('dog_cat.jpg');  
 }
@@ -13,4 +16,21 @@ function draw(){
     noFill();
     stroke("#FF0000");
     rect(30,60,450,350);
+
+    fill("#FF0000");
+    text("Cat",320,120);
+    noFill();
+    stroke("#FF0000");
+    rect(300,90,270,320);
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
 }
