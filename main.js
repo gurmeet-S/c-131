348b8383d467a636c132ae0607a03497eda48be7
Gurmeet Singh <74553130+gurmeet-S@users.noreply.github.com>
2021-07-08 17:20:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
+img = "";
+status = "";
+objects = [];
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
-img = "";
-status = "";
 function preload(){
 img = loadImage('dog_cat.jpg');  
 }
 function draw(){
     image(img,0,0,640,420);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
     fill("#FF0000");
     text("Dog",45,75);
     noFill();
@@ -33,4 +46,5 @@ function gotResult(error, results){
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
